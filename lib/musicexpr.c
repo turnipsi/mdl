d9c94551416117be178409a10ba6e9a40585d67b
Juha Erkkilä <je@turnipsi.no-ip.org>
2015-11-09 22:15:19 +0200

    Forgot to commit this file previously.

---
+++ lib/musicexpr.c
@@ -0,0 +1,34 @@
+/* $Id: musicexpr.c,v 1.1 2015/11/09 20:15:19 je Exp $ */
+
+/*
+ * Copyright (c) 2015 Juha Erkkilä <je@turnipsi.no-ip.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdlib.h>
+
+#include "musicexpr.h"
+
+void
+free_musicexpr(struct musicexpr_t *me)
+{
+	struct musicexpr_t *p, *q;
+
+	p = me;
+	while (p) {
+		q = p;
+		p = p->next;
+		free(q);
+	}
+}
